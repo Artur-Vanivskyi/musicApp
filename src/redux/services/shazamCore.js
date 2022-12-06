2450352b321325c43807a5a82018ce815e92879b
Artur <vanivskyi.artur@thinkful.com>
2022-12-06 18:39:23 -0500

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "de7672e099mshc255d70a64a449bp1ae90djsn43d48726e2aa"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
